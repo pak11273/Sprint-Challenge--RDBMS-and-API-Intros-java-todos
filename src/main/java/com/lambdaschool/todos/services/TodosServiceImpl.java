9579e799bf1b4b95254d537a2f575a48c90d11aa
pak11273 <pak11273@gmail.com>
2021-03-19 16:17:51 -0500

    adds markComplete method

--- src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
+++ src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -1,15 +1,27 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todos;
+import com.lambdaschool.todos.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 @Service(value="todoService")
 public class TodosServiceImpl implements TodosService {
 
+    @Autowired
+    TodoRepository todorepos;
 
+    @Transactional
     @Override
     public void markComplete(long todoid) {
+        Todos todo = todorepos.findById(todoid).orElseThrow(() -> new EntityNotFoundException("Todo " + todoid + " not found."));
+        todo.setCompleted(true);
+
+        todorepos.save(todo);
     }
 }
